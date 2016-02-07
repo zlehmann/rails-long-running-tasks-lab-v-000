262986f63d39920177af300043c18810b1733946
Scott C. Reynolds <scott@scottcreynolds.com>
2016-02-07 17:53:25 -0500

    first draft lab

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -23,13 +23,6 @@ class SongsController < ApplicationController
     end
   end
 
-  def upload
-    CSV.foreach(params["file"].path, headers: true) do |song|
-      Song.create(title: song[0], artist_name: song[1])
-    end
-    redirect_to songs_path
-  end
-
   def edit
     @song = Song.find(params[:id])
   end
