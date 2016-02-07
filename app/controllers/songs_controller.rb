14738f222ba2989a65353275a9a4c9394d35f203
Scott C. Reynolds <scott@scottcreynolds.com>
2016-02-07 17:52:14 -0500

    first draft solution

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,6 @@
 class SongsController < ApplicationController
+  require 'csv'
+
   def index
     @songs = Song.all
   end
@@ -19,6 +21,13 @@ class SongsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def upload
+    CSV.foreach(params["file"].path, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
+    end
+    redirect_to songs_path
   end
 
   def edit
--- spec/features/songs_spec.rb
+++ spec/features/songs_spec.rb
@@ -40,7 +40,10 @@ describe "songs", type:  :feature do
   describe "song uploads" do
     it "can upload a songs file" do
       visit songs_path
-      expect(page).to have_css('file')
+      attach_file("file", Rails.root.join("spec", "fixtures", "songs.csv"))
+      find(:xpath, "//input[contains(@name, 'commit')]").click()
+      song = Song.find_by(title: "Caught Up in You")
+      expect(page).to have_link("Caught Up in You", href: song_path(song))
     end
   end
 end
