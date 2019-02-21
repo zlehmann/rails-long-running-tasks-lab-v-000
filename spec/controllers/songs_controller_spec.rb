bc7da7b4b88fc5101c9ad81e769321afab5c9cc1
maxwellbenton <maxwellbenton@gmail.com>
2019-02-21 17:19:21 -0500

    updates deprecated controller test

--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe SongsController do
     end
 
     it "uploads and processes a file" do
-      post :upload, file: fixture_file_upload('songs.csv', 'text/csv')
+      post :upload, params: { file: fixture_file_upload('songs.csv', 'text/csv') }
       expect(Song.all.count).to eq 13
       expect(Artist.all.count).to eq 6
     end
